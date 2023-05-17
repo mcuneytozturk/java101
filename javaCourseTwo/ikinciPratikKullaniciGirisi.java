e2c178162f8b77473cd2a277cab67fb316bc43b5
Muhsin Cüneyt ÖZTÜRK <mcuneytozturkc@gmail.com>
2023-05-17 12:50:08 +0300

    Create ikinciPratikKullaniciGirisi.java

---
+++ javaCourseTwo/ikinciPratikKullaniciGirisi.java
@@ -0,0 +1,36 @@
+package javaCourseTwo;
+
+import java.util.Scanner;
+
+public class ikinciPratikKullaniciGirisi {
+    public static void main(String[] args) {
+        String userName, passWord, yeniSifre;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Kullanıcı Adınız: ");
+        userName = input.nextLine();
+
+        System.out.print("Şifreniz: ");
+        passWord = input.nextLine();
+
+        if(userName.equals("patika") && passWord.equals("java123")){
+            System.out.println("Kullanıcı adı ve şifre doğru");
+        } else if(userName.equals("patika")) {
+            System.out.println("Şifreniz hatalı!");
+            System.out.print("Şifrenizi değiştirmek isterseniz  evet istemezseniz hayır yazıp enter'a tıklayın: ");
+            yeniSifre = input.nextLine();
+            if(yeniSifre.equals("evet")){
+                System.out.print("Yeni sifreyi giriniz: ");
+                passWord = input.nextLine();
+
+                System.out.println("Şifreniz başarıyla değiştirildi. Sisteme yeni şifreniz ile giriş yapabilirsiniz.");
+            } else {
+                System.out.println("Sifrenizi hatırlayınca gelin.");
+            }
+        } else if (passWord.equals("java123")){
+            System.out.println("Kullanıcı adınız hatalı. Bu konuda bir işlem gerçekleştiremezsiniz. Tekrar deneyiniz");
+        }
+
+    }
+}
