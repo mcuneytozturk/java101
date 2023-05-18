cefd78be9d34957c74341e536a4248c2b54b35ad
Muhsin Cüneyt ÖZTÜRK <mcuneytozturkc@gmail.com>
2023-05-18 10:14:15 +0300

    Update odevArtikYilHesaplama.java

--- javaCourseTwo/odevArtikYilHesaplama.java
+++ javaCourseTwo/odevArtikYilHesaplama.java
@@ -1,7 +1,26 @@
+package javaCourseTwo;
+
+import java.util.Scanner;
+
 /**
  * odevArtikYilHesaplama
  */
 public class odevArtikYilHesaplama {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    
+        System.out.println("Artık Yıl hesaplamak için;");
+        System.out.print("Öğrenmek istediğiniz yılı girin: ");
+
+        int yil = scanner.nextInt();
+
+        if(yil % 4 == 0){
+            if(yil % 100 != 0 || yil % 400 == 0){
+                System.out.println(yil + " bir artık yıldır.");
+            }
+        } else {
+            System.out.println(yil + " bir artık yıl değildir.");
+        }
+    }
+
 }
