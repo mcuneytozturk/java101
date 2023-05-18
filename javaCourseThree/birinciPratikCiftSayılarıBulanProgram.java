0ffdf5511fe6f73d163eddf6a4143eac3ecddd29
Muhsin Cüneyt ÖZTÜRK <mcuneytozturkc@gmail.com>
2023-05-18 11:28:24 +0300

    Create birinciPratikCiftSayılarıBulanProgram.java

---
+++ javaCourseThree/birinciPratikCiftSayılarıBulanProgram.java
@@ -0,0 +1,27 @@
+package javaCourseThree;
+
+import java.util.Scanner;
+
+/**
+ * birinciPratikCiftSayılarıBulanProgram
+ */
+public class birinciPratikCiftSayılarıBulanProgram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Girdiğiniz sayıya kadar olan çift sayılar için;");
+        System.out.print("Bir sayı giriniz: ");
+
+        int sayi = scanner.nextInt();
+
+        for (int i = 0; i < sayi; i++) {
+            if(i % 2 == 0){
+                System.out.println(i + " ");
+            } else {
+                continue;
+            }
+            
+        }
+
+    }
+}
