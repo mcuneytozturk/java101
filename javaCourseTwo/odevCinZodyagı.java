566e514a866446c79df2a304a196bd7b0c2cb9c5
Muhsin Cüneyt ÖZTÜRK <mcuneytozturkc@gmail.com>
2023-05-18 10:01:57 +0300

    Create odevCinZodyagı.java

---
+++ javaCourseTwo/odevCinZodyagı.java
@@ -0,0 +1,60 @@
+package javaCourseTwo;
+
+import java.util.Scanner;
+
+public class odevCinZodyagı {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Doğum Yılınızı Giriniz: ");
+        int dogum = input.nextInt();
+
+        int zodyak = dogum % 12;
+
+        String zodyakBurcu;
+        switch (zodyak) {
+            case 0:
+                zodyakBurcu = "Maymun";
+                break;
+            case 1:
+                zodyakBurcu = "Horoz";
+                break;
+            case 2:
+                zodyakBurcu = "Köpek";
+                break;
+            case 3:
+                zodyakBurcu = "Domuz";
+                break;
+            case 4:
+                zodyakBurcu = "Fare";
+                break;
+            case 5:
+                zodyakBurcu = "Öküz";
+                break;
+            case 6:
+                zodyakBurcu = "Kaplan";
+                break;
+            case 7:
+                zodyakBurcu = "Tavşan";
+                break;
+            case 8:
+                zodyakBurcu = "Ejderha";
+                break;
+            case 9:
+                zodyakBurcu = "Yılan";
+                break;
+            case 10:
+                zodyakBurcu = "At";
+                break;
+            case 11:
+                zodyakBurcu = "Koyun";
+                break;
+            default:
+                zodyakBurcu = "Yanlış değer girildi!";
+        }
+
+        System.out.println("Çin Zodyağı Burcunuz: " + zodyakBurcu);
+
+        input.close();
+    }
+}
