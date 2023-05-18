923d7a0509bb137a0700e4228a1df95a720a87af
Muhsin Cüneyt ÖZTÜRK <mcuneytozturkc@gmail.com>
2023-05-18 12:09:32 +0300

    Update ikinciPratikCiftveDördünKatlariniBulanProgram.java

--- javaCourse3/ikinciPratikCiftveDördünKatlariniBulanProgram.java
+++ javaCourse3/ikinciPratikCiftveDördünKatlariniBulanProgram.java
@@ -13,21 +13,22 @@ public class ikinciPratikCiftveDördünKatlariniBulanProgram {
         System.out.print("Bir sayı giriniz: ");
 
         int sayi = scanner.nextInt();
+
+        while(sayi % 2 == 0) {
+            System.out.println("Lütfen tek sayı giriniz.");
+            sayi = scanner.nextInt();
+        }
         int toplam = 0;
         int bölen = 0;
 
         //ödev kısmı
-        for (int i = 0; i <= sayi; i++) {
-            if (i % 2 == 0 && i % 4 == 0) {
-                toplam += i;
-                bölen++;
-                System.out.println(bölen);
-                System.out.println(toplam);
+            for (int i = 0; i <= sayi; i++) {
+                if (i % 2 == 0 && i % 4 == 0) {
+                    toplam += i;
+                    bölen++;
+                }
             }
-        }
         int ortalama = toplam / bölen;
-
-        
         System.out.println("Çift olan ve 4'e bölünebilen sayıların ortalaması: " + ortalama);
     }
 }
