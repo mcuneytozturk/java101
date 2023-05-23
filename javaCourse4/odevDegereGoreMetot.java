40e713f9eadf469acdc6744afa657035f567f282
Muhsin Cüneyt ÖZTÜRK <mcuneytozturkc@gmail.com>
2023-05-23 17:05:49 +0300

    Update odevDegereGoreMetot.java

--- javaCourse4/odevDegereGoreMetot.java
+++ javaCourse4/odevDegereGoreMetot.java
@@ -6,20 +6,28 @@ public class odevDegereGoreMetot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        //N sayısı alınır
+
         System.out.print("N sayısı: ");
         int n = scanner.nextInt();
         System.out.print("Çıktısı: ");
         manipulateNumber(n);
     }
-    
-    public static void manipulateNumber(int n) {
-        System.out.print(n + " ");
 
+    public static void manipulateNumber(int n) {
+        //N sayısı "Çıktısı : " yanına yazdırılır
+        
+        // sayısı 0'dan küçük ise döndürülür 
         if (n <= 0) {
+            System.out.print(n + " ");   
             return;
         }
-
+        
+        System.out.print(n + " ");   
+        //özyineleme ile N sayısından 5 eksiltilir
         manipulateNumber(n - 5);
+
+        //n sayısı yazdırılır
         System.out.print(n + " ");
     }
 }
