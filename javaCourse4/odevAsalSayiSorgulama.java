4ebe9fab9d566c99c2db54d298afb6de322e4edb
Muhsin Cüneyt ÖZTÜRK <mcuneytozturkc@gmail.com>
2023-05-23 13:54:30 +0300

    yorumlar eklendi

--- javaCourse4/odevAsalSayiSorgulama.java
+++ javaCourse4/odevAsalSayiSorgulama.java
@@ -10,6 +10,7 @@ public class odevAsalSayiSorgulama {
         System.out.print("Sayı giriniz: ");
         int num = scanner.nextInt();
 
+        //burda asal sayı ise true değilse false döndürecek fonksiyon çağrılır ve eğer bildirisinin içine koyulur sonuç yazdırılır
         if(isPrime(num, 2)){
             System.out.println(num + " sayısı ASALDIR");
         } else {
@@ -17,12 +18,17 @@ public class odevAsalSayiSorgulama {
         }
     }
 
+    //asal mı? fonksiyonu yazılır a girilen sayı b bölen olmak üzere iki değer verilir
+
     static boolean isPrime(int a, int b){
+        //değer bölene eşit ise çağırma bitmiş demektir
     if (a == b) {
         return true;
+        //değer bölene eşitlenmeden tam bölünüyorsa çağırma biter false döner
     } else if (a % b == 0) {
         return false;
     } else {
+        //iki koşulda sağlanmıyorsa bölen bir arttıralarak fonksiyon kendini yeniler(recursive)
         return isPrime(a, ++b);
     }
 }
--- javaCourse4/odevUsHesabi.java
+++ javaCourse4/odevUsHesabi.java
@@ -6,19 +6,23 @@ public class odevUsHesabi {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        //değerler istenir
+
         System.out.println("Taban değeri giriniz: ");
         int num = scanner.nextInt();
 
         System.out.println("Üs değerini giriniz: ");
         int pow = scanner.nextInt();
 
+        //sonuç yanında exponential yani üs alma fonksiyonu çağrılarak yazdırılır 2^3 için "Sonuç: 8" gibi bir çıktı alınır
         System.out.println("Sonuç: " + expo(num, pow));
     };
 
     static int expo(int a, int b){
-        
+        //üs değeri 0 ise sonuç 1 olacaktır
         if(b == 0){
             return 1;
+            //değilse taban değer kendisi ile çarpılır ve üs bir azaltılarak fonksiyon yeniden çağrılır
         } else{
             return a * expo(a, b-1); 
         }
